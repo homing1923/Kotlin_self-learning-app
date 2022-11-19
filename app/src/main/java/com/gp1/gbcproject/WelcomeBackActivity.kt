e6a1c0e34b4d45bdcba8bada18d0eed844bc1438
Alvaro Felipe Garcia Mendez <alvarofelipe.garciamendez@georgebrown.ca>
2022-11-19 15:45:06 -0500

    Welcome back and Enter your name screens - Activities added - Missing Styles - Logic added

---
+++ app/src/main/java/com/gp1/gbcproject/WelcomeBackActivity.kt
@@ -0,0 +1,48 @@
+package com.gp1.gbcproject
+
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.system.Os.remove
+import android.util.Log
+import android.view.View
+import com.gp1.gbcproject.databinding.ActivityWelcomeBackBinding
+
+class WelcomeBackActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding: ActivityWelcomeBackBinding
+    lateinit var sharedPrefs: SharedPreferences
+    lateinit var userName: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.continueBtn.setOnClickListener(this)
+        sharedPrefs =
+            this.getSharedPreferences("com_gp1_gbcproject_USER_NAME", MODE_PRIVATE)
+        if (sharedPrefs.contains("KEY_USER_NAME")) {
+            userName = sharedPrefs.getString("KEY_USER_NAME", "").toString()
+            binding.tvWelcomeBack.setText("Welcome back, $userName")
+        }
+    }
+
+    override fun onClick(view: View) {
+        if (view !== null) {
+            when (view.id) {
+                R.id.continue_btn -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.tv_reset -> {
+                    Log.d("asd", "Resettttttt")
+                    with(sharedPrefs.edit()) {
+                        if (sharedPrefs.contains("KEY_USER_NAME")) {
+                            remove("KEY_USER_NAME")
+                            apply()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
