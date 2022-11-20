58c9a2c1fbb2ae82b56859ad2822107d4f51db7a
Alvaro Felipe Garcia Mendez <alvarofelipe.garciamendez@georgebrown.ca>
2022-11-20 14:34:02 -0500

    Tweaking UI

--- app/src/main/java/com/gp1/gbcproject/DataSource.kt
+++ app/src/main/java/com/gp1/gbcproject/DataSource.kt
@@ -3,35 +3,99 @@ package com.gp1.gbcproject
 
 class DataSource {
     private constructor(
-    ){
-        arrayoflesson.add(Lesson("Lesson1", 65, R.drawable.number1,"Demo Lesson 1","http://youtube.com", 1))
-        arrayoflesson.add(Lesson("Lesson2", 125, R.drawable.number2,"Demo Lesson 2","http://youtube.com", 2))
-        arrayoflesson.add(Lesson("Lesson3", 80, R.drawable.number3,"Demo Lesson 3","http://youtube.com", 3))
-        arrayoflesson.add(Lesson("Lesson4", 45, R.drawable.number4,"Demo Lesson 4","http://youtube.com", 4))
-        arrayoflesson.add(Lesson("Lesson5", 95, R.drawable.number5,"Demo Lesson 5","http://youtube.com", 5))
-        arrayoflesson.add(Lesson("Lesson6", 120, R.drawable.number6,"Demo Lesson 6","http://youtube.com", 6))
-        arrayoflesson.add(Lesson("Lesson7", 55, R.drawable.number7,"Demo Lesson 7","http://youtube.com", 7))
+    ) {
+        arrayoflesson.add(
+            Lesson(
+                "Getting Started",
+                65,
+                R.drawable.number1,
+                "First things first! What is Angular? Why would you want to learn it? This lecture helps answering this question.",
+                "http://youtube.com",
+                1
+            )
+        )
+        arrayoflesson.add(
+            Lesson(
+                "The Basics",
+                125,
+                R.drawable.number2,
+                "We saw our first App run in the browser but do you really know how it got there? Angular is all about Components! This lectures takes a closer look and explains what Components really are.",
+                "http://youtube.com",
+                2
+            )
+        )
+        arrayoflesson.add(
+            Lesson(
+                "Debugging",
+                80,
+                R.drawable.number3,
+                "Things don't always go the way you want them to go. Learn how to read Angular's error messages and how incredibly useful can be to debug your app in the browser - learn more in this lecture.",
+                "http://youtube.com",
+                3
+            )
+        )
+        arrayoflesson.add(
+            Lesson(
+                "Components and Data-binding",
+                45,
+                R.drawable.number4,
+                "You're not limited to binding to built-in properties. Indeed, binding to custom property is a key feature of Angular apps. As well, this lecture explains how you may split an existing app into multiple new components.",
+                "http://youtube.com",
+                4
+            )
+        )
+        arrayoflesson.add(
+            Lesson(
+                "Directives",
+                95,
+                R.drawable.number5,
+                "When using directives, you have an easy way of reacting to events on your hosting element. Learn more about it in this lecture.",
+                "http://youtube.com",
+                5
+            )
+        )
+        arrayoflesson.add(
+            Lesson(
+                "Using Services and Dependency Injection",
+                120,
+                R.drawable.number6,
+                "Services are a core concept of Angular - let me explain what you may expect from this section.",
+                "http://youtube.com",
+                6
+            )
+        )
+        arrayoflesson.add(
+            Lesson(
+                "Changing pages with Routing",
+                55,
+                R.drawable.number7,
+                "Thus far, we didn't really hide that we're building a single page application. Let's give the user the feeling of switching pages - with the Angular router!",
+                "http://youtube.com",
+                7
+            )
+        )
     }
 
-    companion object{
+    companion object {
         @Volatile
-        private lateinit var instance:DataSource
+        private lateinit var instance: DataSource
 
-        fun getInstance(): DataSource{
-            synchronized(this){
-                if(!::instance.isInitialized){
+        fun getInstance(): DataSource {
+            synchronized(this) {
+                if (!::instance.isInitialized) {
                     instance = DataSource()
                 }
                 return instance
             }
         }
     }
-    var username:String? = null
-    val arrayoflesson:ArrayList<Lesson> = arrayListOf()
-    var milestone:Int = 0
-    var currentLesson:Lesson? = null
-    var currentLessonid:Int = -1
-    var finishedlessonlist:MutableList<Int> = mutableListOf()
-    var finishedlessonset:MutableSet<Int> = mutableSetOf()
-    var notearray:MutableList<String> = mutableListOf()
+
+    var username: String? = null
+    val arrayoflesson: ArrayList<Lesson> = arrayListOf()
+    var milestone: Int = 0
+    var currentLesson: Lesson? = null
+    var currentLessonid: Int = -1
+    var finishedlessonlist: MutableList<Int> = mutableListOf()
+    var finishedlessonset: MutableSet<Int> = mutableSetOf()
+    var notearray: MutableList<String> = mutableListOf()
 }
